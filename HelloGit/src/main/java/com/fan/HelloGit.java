97a91ea5e0144702c4d03080eb7e3eba2b4d0dc5
Tony <tony@gmail.com>
2020-10-30 14:35:47 +0800

    HelloGit

---
+++ HelloGit/src/main/java/com/fan/HelloGit.java
@@ -0,0 +1,11 @@
+package com.fan;
+
+/**
+ * @author fanzhenxin
+ * @date 2020-10-30 14:26
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
